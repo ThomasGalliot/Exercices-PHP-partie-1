ac6dc628a4013db934e9c5db204413b5d7409b29
ThomasGalliot <thomas.galliot@hotmail.fr>
2018-07-23 13:47:43 +0200

    azerty

---
+++ exercice.php
@@ -0,0 +1,67 @@
+<?php
+
+    $name = 'thomas <br><br>';
+    
+    echo $name;
+
+?>
+
+<?php
+
+    $lastname = 'GALLIOT';
+    $firstname = 'Thomas';
+    $age = 20;
+    
+    echo $lastname . " " . $firstname . " " . $age . "<br><br>";
+
+?>
+
+<?php
+
+    $km = 1;
+    echo $km . " ";
+
+    $km += 3;
+    echo $km . " ";
+
+    $km += 125;
+    echo $km . "<br><br>";
+
+?>
+
+<?php
+
+    $int = NULL;
+    echo $int . " ";
+
+    $int = 100;
+    echo $int . "<br><br>";
+
+?>
+
+<?php
+
+    $name = 'Thomas';
+    echo "Bonjour " . $name . ", comment va tu?<br><br>";
+
+?>
+
+<?php
+
+    $lastname = 'GALLIOT';
+    $firstname = 'Thomas';
+    $age = 20;
+
+    echo "Bonjour " . $firstname . " " . $lastname . ", tu as " . $age . " ans.<br><br>";
+
+?>
+
+<?php
+
+    $addition = 3 + 4;
+    $multiplication = 5 * 20;
+    $division = 45 / 5;
+
+    echo $addition . " " . $multiplication . " " . $division;
+
+?>
